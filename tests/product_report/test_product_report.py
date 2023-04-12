93fdfb631c8b0cc8f867fcc931eb1392672b1695
Bianca Oura <biancaoura@gmail.com>
2023-04-11 21:46:34 -0300

    test: add product report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "fentanyl citrate",
+        "Target Corporation",
+        "2020-12-06",
+        "2023-12-25",
+        "FR29 5951 7573 74OY XKGX 6CSG D20",
+        "instrucao 2",
+    )
+
+    assert str(product) == (
+        "O produto fentanyl citrate"
+        " fabricado em 2020-12-06"
+        " por Target Corporation com validade"
+        " até 2023-12-25"
+        " precisa ser armazenado instrucao 2."
+    )
